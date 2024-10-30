df11609771745da092b731a03460a8d48167044b
Elinor Morris <147081105+ELIIMORRIS@users.noreply.github.com>
2024-10-30 16:44:09 +0000

    updated all headers to be dynamically loaded on each page, using dict in app.py

--- app.py
+++ app.py
@@ -3,61 +3,84 @@ import os
 
 app = Flask(__name__)
 
+# Centralised headers dictionary
+headers = {
+    'home': "Data and Computational Thinking",
+    'computational_thinking': "Computational Thinking",
+    'data_format': "Formatting Data",
+    'evaluate_data': "Extract and Evaluate Information from Data",
+    'data_sets': "Data Sets",
+    'spreadsheet_formulae': "Spreadsheet Formulae",
+    'break_down_a_problem': "Decomposition: Break Down a Problem",
+    'debugging': "Debugging: Detect and Correct Mistakes",
+    'instructions_test_ideas': "Create + Record Instructions to Test Ideas",
+    'instructions_change': "Change Instructions to Achieve a Different Outcome",
+    'identify_repetitions': "Identify Repetitions in a Sequence",
+    'refining_algorithms': "Refining Algorithms",
+    'importance_of_order': "Importance of Order of Statements within Algorithms",
+    'resources': "Resources"
+}
+
+# Context processor to make headers dictionary available globally
+@app.context_processor
+def inject_headers():
+    return dict(headers=headers)
+
 # Home route for landing page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', header=headers['home'])
 
 # Route for each topic page.  trailing slash '/url/' added for following. if URL accessed w/o trailing slash, Flask will redirect to the canonical URL with the trailing slash
 @app.route('/data_literacy/data_format')
 def data_format():
-    return render_template('data_literacy/data_format.html')
+    return render_template('data_literacy/data_format.html', header=headers['data_format'])
 
 @app.route('/data_literacy/evaluate_data')
 def evaluate_data():
-    return render_template('data_literacy/evaluate_data.html')
+    return render_template('data_literacy/evaluate_data.html', header=headers['evaluate_data'])
 
 @app.route('/data_literacy/data_sets')
 def data_sets():
-    return render_template('data_literacy/data_sets.html')
+    return render_template('data_literacy/data_sets.html', header=headers['data_sets'])
 
 @app.route('/data_literacy/spreadsheet_formulae')
 def spreadsheet_formulae():
-    return render_template('data_literacy/spreadsheet_formulae.html')
+    return render_template('data_literacy/spreadsheet_formulae.html', header=headers['spreadsheet_formulae'])
 
 # Routes for 'problem_solving' pages
 @app.route('/problem_solving/break_down_a_problem')
 def break_down_a_problem():
-    return render_template('problem_solving/break_down_a_problem.html')
+    return render_template('problem_solving/break_down_a_problem.html', header=headers['break_down_a_problem'])
 
 @app.route('/problem_solving/debugging')
 def debugging():
-    return render_template('problem_solving/debugging.html')
+    return render_template('problem_solving/debugging.html', header=headers['debugging'])
 
 @app.route('/problem_solving/instructions_test_ideas')
 def instructions_test_ideas():
-    return render_template('problem_solving/instructions_test_ideas.html')
+    return render_template('problem_solving/instructions_test_ideas.html', header=headers['instructions_test_ideas'])
 
 @app.route('/problem_solving/instructions_change')
 def instructions_change():
-    return render_template('problem_solving/instructions_change.html')
+    return render_template('problem_solving/instructions_change.html', header=headers['instructions_change'])
 
 @app.route('/problem_solving/identify_repetitions')
 def identify_repetitions():
-    return render_template('problem_solving/identify_repetitions.html')
+    return render_template('problem_solving/identify_repetitions.html', header=headers['identify_repetitions'])
 
 @app.route('/problem_solving/refining_algorithms')
 def refining_algorithms():
-    return render_template('problem_solving/refining_algorithms.html')
+    return render_template('problem_solving/refining_algorithms.html', header=headers['refining_algorithms'])
 
 @app.route('/problem_solving/importance_of_order')
 def importance_of_order():
-    return render_template('problem_solving/importance_of_order.html')
+    return render_template('problem_solving/importance_of_order.html', header=headers['importance_of_order'])
 
 # Routes for other pages
 @app.route('/computational_thinking')
 def computational_thinking():
-    return render_template('computational_thinking.html')
+    return render_template('computational_thinking.html', header=headers['computational_thinking'])
 
 @app.route('/download/<filename>')
 def download_file(filename):
@@ -70,7 +93,7 @@ def download_file(filename):
 
 @app.route('/resources')
 def resources():
-    return render_template('resources.html')
+    return render_template('resources.html', header=headers['resources'])
 
 if __name__ == '__main__':
     app.run(debug=True)
