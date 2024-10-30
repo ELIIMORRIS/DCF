7ebdb0fbc16540cdc416d3b32fa5af2436271354
Elinor Morris <147081105+ELIIMORRIS@users.noreply.github.com>
2024-10-30 22:22:03 +0000

    sample drag and drop - importance of order html

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,38 @@ document.querySelectorAll('.collapsible').forEach(button => {
         }
     });
 });
+
+const items = document.querySelectorAll('.item');
+const lists = document.querySelectorAll('.list');
+
+items.forEach(item => {
+    item.addEventListener('dragstart', dragStart);
+    item.addEventListener('dragend', dragEnd);
+});
+
+lists.forEach(list => {
+    list.addEventListener('dragover', dragOver);
+    list.addEventListener('drop', drop);
+});
+
+function dragStart(e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    setTimeout(() => {
+        e.target.classList.add('hide');
+    }, 0);
+}
+
+function dragEnd(e) {
+    e.target.classList.remove('hide');
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    e.preventDefault();
+    const id = e.dataTransfer.getData('text/plain');
+    const draggable = document.getElementById(id);
+    e.target.appendChild(draggable);
+}
